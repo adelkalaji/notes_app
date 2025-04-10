d06cd5dffed6dfbfb91129e863aaa9b18b8027e2
adelkalaji <adel.kalaji1998@gmail.com>
2025-04-10 18:45:11 +0300

    edit note done

--- notes_app/lib/views/widgets/custom_app_bar.dart
+++ notes_app/lib/views/widgets/custom_app_bar.dart
@@ -2,17 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key, required this.title, required this.icon});
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    required this.icon,
+    this.onPressed,
+  });
 
+  final void Function()? onPressed;
   final String title;
   final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(title, style: TextStyle(fontSize: 28)),
         Spacer(),
-        CustomIcon(icon: Icons.search),
+        CustomIcon(icon: icon, onPressed: onPressed),
       ],
     );
   }
--- notes_app/lib/views/widgets/custom_icon.dart
+++ notes_app/lib/views/widgets/custom_icon.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CustomIcon extends StatelessWidget {
-  const CustomIcon({super.key, required this.icon});
+  const CustomIcon({super.key, required this.icon, this.onPressed});
 
+  final void Function()? onPressed;
   final IconData icon;
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,7 @@ class CustomIcon extends StatelessWidget {
         color: Colors.white.withOpacity(.05),
         borderRadius: BorderRadius.circular(16),
       ),
-      child: IconButton(onPressed: () {}, icon: Icon(icon, size: 28)),
+      child: IconButton(onPressed: onPressed, icon: Icon(icon, size: 28)),
     );
   }
 }
